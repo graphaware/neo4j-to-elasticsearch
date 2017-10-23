2b58667ce898f26cc97799406e134f25381269a5
davidrapin <david.rapin@gmail.com>
2017-10-23 14:23:51 -0400

    #86 fixed default request timeout in JEST client (changed to 20 seconds)

--- src/main/java/com/graphaware/module/es/ElasticSearchWriter.java
+++ src/main/java/com/graphaware/module/es/ElasticSearchWriter.java
@@ -52,11 +52,12 @@ public class ElasticSearchWriter extends BaseThirdPartyWriter {
     private final String authUser;
     private final String authPassword;
     private final Mapping mapping;
+    private final boolean async;
 
     public ElasticSearchWriter(ElasticSearchConfiguration configuration) {
         super(configuration.getQueueCapacity());
 
-        notNull(configuration);
+        notNull(configuration, "Configuration cannot be null");
 
         this.uri = configuration.getUri();
         this.port = configuration.getPort();
@@ -65,6 +66,7 @@ public class ElasticSearchWriter extends BaseThirdPartyWriter {
         this.authUser = configuration.getAuthUser();
         this.authPassword = configuration.getAuthPassword();
         this.mapping = configuration.getMapping();
+        this.async = configuration.isAsyncIndexation();
     }
 
     /**
@@ -101,38 +103,48 @@ public class ElasticSearchWriter extends BaseThirdPartyWriter {
     protected void processOperations(List<Collection<WriteOperation<?>>> operationGroups) {
         createIndexIfNotExist();
 
-        OperationExecutor executor = executorFactory.newExecutor(client);
+        Runnable update = () -> {
+            OperationExecutor executor = executorFactory.newExecutor(client);
 
-        executor.start();
+            executor.start();
 
-        int actionsCount = 0;
-        for (Collection<WriteOperation<?>> operationGroup : operationGroups) {
-            for (WriteOperation<?> operation : operationGroup) {
-                List<BulkableAction<? extends JestResult>> actions = mapping.getActions(operation);
-                executor.execute(actions, operation);
-                actionsCount += actions.size();
-            }
-        }
-
-        if (actionsCount == 0) {
-            return;
-        }
-
-        List<WriteOperation<?>> allFailed = executor.flush();
-
-        if (!allFailed.isEmpty()) {
-            if (retryOnError) {
-                LOG.warn("There were " + allFailed.size() + " failures in replicating to Elasticsearch. Will retry...");
-                retry(Collections.singletonList(allFailed));
-                try {
-                    LOG.info("Backing off for 2 seconds...");
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    LOG.warn("Wait interrupted", e);
+            int actionsCount = 0;
+            for (Collection<WriteOperation<?>> operationGroup : operationGroups) {
+                for (WriteOperation<?> operation : operationGroup) {
+                    List<BulkableAction<? extends JestResult>> actions = mapping.getActions(operation);
+                    executor.execute(actions, operation);
+                    actionsCount += actions.size();
                 }
-            } else {
-                LOG.warn("There were " + allFailed.size() + " failures in replicating to Elasticsearch. These updates got lost.");
             }
+
+            if (actionsCount == 0) {
+                return;
+            }
+
+            List<WriteOperation<?>> allFailed = executor.flush();
+
+            if (!allFailed.isEmpty()) {
+                if (retryOnError) {
+                    LOG.warn("There were " + allFailed.size() + " failures in replicating to Elasticsearch. Will retry...");
+                    retry(Collections.singletonList(allFailed));
+                    try {
+                        LOG.info("Backing off for 2 seconds...");
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        LOG.warn("Wait interrupted", e);
+                    }
+                } else {
+                    LOG.warn("There were " + allFailed.size() + " failures in replicating to Elasticsearch. These updates got lost.");
+                }
+            }
+        };
+
+        if (async) {
+            Thread updateThread = new Thread(update, "Elasticsearch index update");
+            updateThread.setUncaughtExceptionHandler((t, e) -> LOG.error("Index update error", e));
+            updateThread.start();
+        } else {
+            update.run();
         }
     }
 
--- src/main/java/com/graphaware/module/es/util/CustomJestClientFactory.java
+++ src/main/java/com/graphaware/module/es/util/CustomJestClientFactory.java
@@ -1,7 +1,10 @@
 package com.graphaware.module.es.util;
 
 import io.searchbox.client.JestClientFactory;
+import io.searchbox.client.config.HttpClientConfig;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.protocol.HttpRequestExecutor;
 
 public class CustomJestClientFactory extends JestClientFactory {
 
@@ -9,5 +12,13 @@ public class CustomJestClientFactory extends JestClientFactory {
     protected HttpClientBuilder configureHttpClient(HttpClientBuilder builder) {
         return builder
                 .setConnectionManagerShared(true);
+    }
+
+    @Override
+    public void setHttpClientConfig(HttpClientConfig httpClientConfig) {
+        super.setHttpClientConfig(new HttpClientConfig.Builder(httpClientConfig)
+                .readTimeout(20000)
+                .build()
+        );
     }
 }
